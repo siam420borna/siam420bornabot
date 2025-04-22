d29f52529ff54567f38a3a9be46c6dbb2879f8de
siam420borna <siam420borna@gmail.com>
2025-04-22 12:19:16 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,20 +1,22 @@
+import os
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from utils import create_welcome_image
-import os
 
-API_ID = int(os.getenv("API_ID"))
-API_HASH = os.getenv("API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
+API_ID = int(os.environ.get("API_ID"))
+API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
 
-bot = Client("welcome-bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
+app = Client("welcome-bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
-
-@bot.on_message(filters.new_chat_members)
-async def welcome_new_member(_, message: Message):
+@app.on_message(filters.new_chat_members)
+async def welcome(_, message: Message):
     for user in message.new_chat_members:
-        photo = await bot.download_media(user.photo.big_file_id) if user.photo else "default.jpg"
-        image = create_welcome_image(photo, user.first_name, user.username, user.id)
-        await message.reply_photo(image, caption=f"Welcome {user.mention}!")
+        img = await create_welcome_image(user)
+        await message.reply_photo(
+            photo=img,
+            caption=f"স্বাগতম {user.mention}!\n\nইউজারনেম: @{user.username or 'N/A'}\nইউজার আইডি: `{user.id}`"
+        )
+        img.close()
 
-bot.run()
+app.run()
