96b75cb00a871ddf2bf991da95751d2c0a341a89
siam420borna <siam420borna@gmail.com>
2025-04-22 11:34:01 +0600

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+import os
+
+class Config:
+    API_ID = int(os.getenv("API_ID", "123456"))
+    API_HASH = os.getenv("API_HASH", "your_api_hash")
+    BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")
+    DEFAULT_IMG = "default.jpg"
